d47bc11db8574139957e631560f45dc0c7144321
maksgir <maksgirya@mail.ru>
2023-02-28 02:21:35 +0300

    secant method

--- lab2/src/main/java/com/maksgir/lab2/math/SecantMethod.java
+++ lab2/src/main/java/com/maksgir/lab2/math/SecantMethod.java
@@ -7,6 +7,26 @@ import com.maksgir.lab2.equation.Equation;
 public class SecantMethod implements SolutionMethod {
     @Override
     public Answer solveEquation(Equation equation, Interval interval, double epsilon) {
-        return null;
+
+        double x0 = interval.getA();
+        double x1 = interval.getB();
+        int i = 0;
+        double x2 = 0;
+
+        do {
+            i++;
+            x2 = x1 - ((x1 - x0) / (equation.f(x1) - equation.f(x0)) * equation.f(x1));
+
+            double m = Math.abs(x2 - x1);
+            System.out.println(x0 + " " + x1 + " " + x2);
+            if (m <= epsilon) {
+                break;
+            }
+            x0 = x1;
+            x1 = x2;
+
+        } while (true);
+
+        return new Answer(x2, i);
     }
 }
